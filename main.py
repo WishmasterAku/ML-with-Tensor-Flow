16493eb8d6b16f6be17969542df7f7f85cb75272
WishmasterAku <xandertoolz@gmail.com>
2023-04-19 21:25:01 -0500

    Model class

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np 
 
-m = 2
-b = 0.5
-x = np.linspace(0,4,100)
-y = m * x + b + np.random.randn(*x.shape) * 0.5
+m = 2# Slope
+b = 0.5# Intercept
+x = np.linspace(0,4,100)# Create 100 points between 0 and 4
+y = m * x + b + np.random.randn(*x.shape) * 0.5# Add some noise
 
-plt.scatter(x,y)
+
+
+plt.scatter(x,y)# Plot the data
+
+class Model:
+    def __init__(self):
+        self.weight = tf.Variable(10)# Create a variable with an initial value of 10
+        self.bias = tf.Variable(10)
+        
+    def __call__(self, x):# This is the function that will be called when we call the class
+        return self.weight * x + self.bias
+        #self.weight.assign_sub(15.0)
+        
+    
