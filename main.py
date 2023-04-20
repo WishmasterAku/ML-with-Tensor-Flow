0d89532a9604b62f20176eb986ecd5ff16c33dc0
WishmasterAku <xandertoolz@gmail.com>
2023-04-19 20:10:36 -0500

    Linear Regression Plot

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np 
+
+m = 2
+b = 0.5
+x = np.linspace(0,4,100)
+y = m * x + b + np.random.randn(*x.shape) * 0.5
+
+plt.scatter(x,y)
